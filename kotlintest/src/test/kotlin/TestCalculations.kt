32d643d46758c9be6b3b11a7c3d28597dfd1c71e
Martin Skarsaune <martin@skarsaune.net>
2025-06-11 15:56:57 +0200

    Further developments

--- kotlintest/src/test/kotlin/TestCalculations.kt
+++ kotlintest/src/test/kotlin/TestCalculations.kt
@@ -3,50 +3,40 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
 import org.junit.jupiter.params.provider.CsvSource
+import java.util.stream.Stream
 
 class TestCalculations {
     @TestFactory
     fun dynamicTestMultiplication(): List<DynamicTest> {
 
-        data class TestCase(
-            val name: String,
-            val givenFirstFactor: Int,
-            val givenSecondFactor: Int,
-            val thenExpectResult: Int
-        )
-
         println("Before tests")
-        val testCases = arrayOf(
-            TestCase(
-                name = "One by two is two",
-                givenFirstFactor = 1,
-                givenSecondFactor = 2,
-                thenExpectResult = 2
-            ),
-            TestCase(
-                name = "Two by two is four",
-                givenFirstFactor = 2,
-                givenSecondFactor = 2,
-                thenExpectResult = 4
-            ),
-        )
-        val tests = testCases.map { testCase ->
-            dynamicTest(testCase.name) {
-                println("Running test ${testCase.name}")
-                Assertions.assertEquals(
-                    testCase.thenExpectResult,
-                    multiply(testCase.givenFirstFactor, testCase.givenSecondFactor)
-                )
+        val tests = testCases().map { testCase ->
+            dynamicTest(testCase.toString()) {
+                runTestCase(testCase)
             }
         }
         println("After tests")
         return tests
     }
 
+    data class TestCase(
+        val givenFirstFactor: Int,
+        val givenSecondFactor: Int,
+        val thenExpectResult: Int
+    ) {
+        override fun toString():String = "Verify that $givenFirstFactor * $givenSecondFactor = $thenExpectResult"
+    }
+
+
+
     @ParameterizedTest()
-//    @ParameterizedTest(name = "Verifying multiplication of {0} * {1} = {2}")
+//    @ParameterizedTest(name = "Verifying that multiplication of {0} * {1} = {2}")
     @CsvSource(
         value =
         [
@@ -61,4 +51,42 @@ class TestCalculations {
             multiply(givenFirstFactor, givenSecondFactor)
         )
     }
+
+    class TestParameters : ArgumentsProvider {
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> {
+            return testCases().map { Arguments.of(it)}.stream()
+        }
+    }
+
+    @ParameterizedTest()
+    @ArgumentsSource(TestParameters::class)
+    fun runTestCase(testCase: TestCase) {
+        println("Running test ${testCase}")
+        Assertions.assertEquals(
+            testCase.thenExpectResult,
+            multiply(testCase.givenFirstFactor, testCase.givenSecondFactor)
+        )
+    }
+
+    companion object {
+        fun testCases() = arrayOf(
+            TestCase(
+                givenFirstFactor = 1,
+                givenSecondFactor = 2,
+                thenExpectResult = 2
+            ),
+            TestCase(
+                givenFirstFactor = 2,
+                givenSecondFactor = 2,
+                thenExpectResult = 4
+            ),
+            TestCase(
+                givenFirstFactor = 2,
+                givenSecondFactor = 3,
+                thenExpectResult = 6
+            ),
+        )
+    }
+
+
 }
